24bf7047559bc1ec2cb77c582c65a6e6f5fbe295
Ray <rayzlui@gmail.com>
2019-09-09 17:31:16 -0700

    add new game action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,9 @@
-import { INPUT_LETTER } from './actionTypes';
+import { INPUT_LETTER, NEW_GAME } from './actionTypes';
 
 export function inputLetter(letter) {
   return { type: INPUT_LETTER, letter: letter };
+}
+
+export function newGame() {
+  return { type: NEW_GAME };
 }
